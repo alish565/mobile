050ef1d5f80de8af50cc224937c78a8ddda28a34
alish565 <122810148+alish565@users.noreply.github.com>
2024-12-02 15:42:28 +0200

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,143 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(GuessingGameApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class GuessingGameApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: GuessingGame(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class GuessingGame extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _GuessingGameState createState() => _GuessingGameState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _GuessingGameState extends State<GuessingGame> {
+  final int minNum = 1;
+  final int maxNum = 100;
+  late int answer;
+  int attempts = 0;
+  final TextEditingController guessController = TextEditingController();
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    _resetGame();
+  }
+
+  void _resetGame() {
+    answer = Random().nextInt(maxNum - minNum + 1) + minNum;
+    attempts = 0;
+  }
+
+  void _checkGuess(String input) {
+    int? guess = int.tryParse(input);
+
+    if (guess == null) {
+      _showAlert("Invalid Input", "Please enter a valid number.");
+      return;
+    }
+
+    if (guess < minNum || guess > maxNum) {
+      _showAlert("Out of Range", "Please guess a number between $minNum and $maxNum.");
+      return;
+    }
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      attempts++;
+      if (guess < answer) {
+        _showAlert("Too Low", "Your guess is too low. Try again!");
+      } else if (guess > answer) {
+        _showAlert("Too High", "Your guess is too high. Try again!");
+      } else {
+        _showAlert(
+          "Correct!",
+          "The answer was $answer. It took you $attempts attempts.",
+          onDismiss: () {
+            _resetGame();
+            setState(() {});
+          },
+        );
+      }
     });
+  }
+
+  void _showAlert(String title, String message, {VoidCallback? onDismiss}) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(title,style: TextStyle(fontSize: 25),),
+        content: Text(message,style: TextStyle(fontSize: 18),),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+              if (onDismiss != null) onDismiss();
+            },
+            child: Text("OK"),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Colors.blueGrey,
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        backgroundColor: Colors.lightBlueAccent,
+        title: Text("Guess the Number", style: TextStyle(color: Colors.white)),
+        centerTitle: true,
+
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+          children: [
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              "Guess a number between $minNum and $maxNum",
+              style: TextStyle(fontSize: 35, color: Colors.white),
+              textAlign: TextAlign.center,
+
+            ),
+            SizedBox(height: 16, width: 50,),
+
+              SizedBox(width: 200.0, height: 50.0,
+                child:TextField(
+              controller: guessController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Your Guess",
+                labelStyle: TextStyle(color: Colors.white),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black),
+                ),
+              ),
+            ),
+              ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                _checkGuess(guessController.text);
+                guessController.clear();
+              },
+              child: Text("Enter", style: TextStyle(fontSize: 18)),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
